8f6ac93664276bdf255eb9ef26950b733c38f4ad
yqchilde <yqchilde@gmail.com>
2020-03-16 13:01:20 +0800

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -2,14 +2,13 @@ const AV = require('leanengine');
 const mail = require('./utilities/send-mail');
 const Comment = AV.Object.extend('Comment');
 const request = require('request');
-AV.Cloud.afterSave('Comment', function (request) {
-    let currentComment = request.object;
 
+function sendNotification(currentComment) {
     // 通知站长
     mail.notice(currentComment);
-    
+
     // AT评论通知
-    let pid =currentComment.get('pid');
+    let pid = currentComment.get('pid');
 
     if (!pid) {
         console.log("这条评论没有 @ 任何人");
@@ -27,30 +26,36 @@ AV.Cloud.afterSave('Comment', function (request) {
     }, function (error) {
         console.warn('好像 @ 了一个不存在的人!!!');
     });
+}
+
+AV.Cloud.afterSave('Comment', function (request) {
+    let currentComment = request.object;
+
+    return sendNotification(currentComment)
 });
 
-AV.Cloud.define('resend_mails', function(req) {
+AV.Cloud.define('resend_mails', function (req) {
     let query = new AV.Query(Comment);
-    query.greaterThanOrEqualTo('createdAt', new Date(new Date().getTime() - 24*60*60*1000));
+    query.greaterThanOrEqualTo('createdAt', new Date(new Date().getTime() - 24 * 60 * 60 * 1000));
     query.notEqualTo('isNotified', true);
     // 如果你的评论量很大，可以适当调高数量限制，最高1000
     query.limit(200);
-    return query.find().then(function(results) {
-        new Promise((resolve, reject)=>{
+    return query.find().then(function (results) {
+        new Promise((resolve, reject) => {
             count = results.length;
-            for (var i = 0; i < results.length; i++ ) {
-                sendNotification(results[i], req.meta.remoteAddress);
+            for (var i = 0; i < results.length; i++) {
+                sendNotification(results[i]);
             }
             resolve(count);
-        }).then((count)=>{
+        }).then((count) => {
             console.log(`昨日${count}条未成功发送的通知邮件处理完毕！`);
-        }).catch((e)=>{
+        }).catch((e) => {
             console.log('错误日志:' + e);
         });
     });
 });
 
-AV.Cloud.define('self_wake', function(req) {
+AV.Cloud.define('self_wake', function (req) {
     request(process.env.ADMIN_URL, function (error, response, body) {
         console.log('自唤醒任务执行成功，响应状态码为:', response && response.statusCode);
     });
